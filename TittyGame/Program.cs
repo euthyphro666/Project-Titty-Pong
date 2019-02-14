739c1c8b404f69e38ad64962a2eb067f0c7bfe98
Josh <joshhess13@gmail.com>
2019-02-13 21:56:28 -0700

    Renamed some shit

--- TittyGame/Program.cs
+++ TittyGame/Program.cs
@@ -14,7 +14,7 @@ namespace TittyGame
         [STAThread]
         static void Main()
         {
-            using (var game = new Game1())
+            using (var game = new Master())
                 game.Run();
         }
     }
