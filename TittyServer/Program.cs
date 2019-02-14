c2e98625828706c5e2a6980d04687489f0599c10
Jared Ross <jdross91@gmail.com>
2019-02-13 21:55:32 -0700

    Update some names
    Took 1 hour 2 minutes

--- TittyServer/Program.cs
+++ TittyServer/Program.cs
@@ -14,7 +14,7 @@ namespace TittyServer
         [STAThread]
         static void Main()
         {
-            using (var game = new Game1())
+            using (var game = new ServerGameManager())
                 game.Run();
         }
     }
--- TittyServer/ServerGameManager.cs
+++ TittyServer/ServerGameManager.cs
@@ -7,12 +7,12 @@ namespace TittyServer
     /// <summary>
     /// This is the main type for your game.
     /// </summary>
-    public class Game1 : Game
+    public class ServerGameManager : Game
     {
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
 
-        public Game1()
+        public ServerGameManager()
         {
             graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
