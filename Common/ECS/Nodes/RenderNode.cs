3ec96c1d50a08da76bc4fddff5e107193daed5da
Josh <joshhess13@gmail.com>
2019-02-19 20:24:20 -0700

    More render changes.

--- Common/ECS/Nodes/RenderNode.cs
+++ Common/ECS/Nodes/RenderNode.cs
@@ -1,4 +1,5 @@
-﻿using Common.ECS.Contracts;
+﻿using Common.ECS.Components;
+using Common.ECS.Contracts;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,5 +10,8 @@ namespace Common.ECS.Nodes
 {
     public class RenderNode : INode
     {
+        public DisplayComponent Display { get; set; }
+        public PositionComponent Position { get; set; }
+        public RigidBodyComponent RigidBody { get; set; }
     }
 }
--- Common/ECS/Systems/RenderSystem.cs
+++ Common/ECS/Systems/RenderSystem.cs
@@ -14,7 +14,7 @@ namespace Common.ECS.Systems
 
         public void Update()
         {
-            throw new NotImplementedException();
+            
         }
     }
 }
