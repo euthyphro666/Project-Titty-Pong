02b031457cfaba60281bf7af7d2158bb7d78dd63
Josh <joshhess13@gmail.com>
2019-02-19 19:27:18 -0700

    Changed physics systems to be distinct systems.

--- Common/ECS/Systems/CollisionSystem.cs
+++ Common/ECS/Systems/CollisionSystem.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Common.ECS.Systems
 {
-    class PhysicsSystem
+    class CollisionSystem
     {
     }
 }
--- Common/ECS/Systems/MovementSystem.cs
+++ Common/ECS/Systems/MovementSystem.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Common.ECS.Systems
 {
-    public class InputSystem
+    class MovementSystem
     {
     }
 }
