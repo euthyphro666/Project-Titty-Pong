1d86d9595e8278cd2fe5f93cfcca493ce35c4270
Josh <joshhess13@gmail.com>
2019-02-19 19:00:53 -0700

    Added some components and systems.

---
+++ Common/ECS/Components/DisplayComponent.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.ECS.Components
+{
+    public class DisplayComponent
+    {
+        public Texture2D Sprite;
+    }
+}
---
+++ Common/ECS/Components/PositionComponent.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.ECS.Components
+{
+    public class PositionComponent
+    {
+        public float X;
+        public float Y;
+    }
+}
