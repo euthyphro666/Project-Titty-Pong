64e8270fa2d51f24c9d07710e3ad8cf5ef6b79f5
Josh <joshhess13@gmail.com>
2019-02-19 20:13:10 -0700

    Added contracts, engine, and render node.

---
+++ Common/ECS/Engine.cs
@@ -0,0 +1,24 @@
+﻿using Common.ECS.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.ECS
+{
+    public class Engine
+    {
+
+        private List<ISystem> Systems { get; set; }
+        private List<Entity> Entities { get; set; }
+
+        public Engine()
+        {
+
+        }
+
+
+
+    }
+}
--- Common/ECS/Systems/RenderSystem.cs
+++ Common/ECS/Systems/RenderSystem.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Common.ECS.Contracts;
+using Common.ECS.Nodes;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,13 @@ using System.Threading.Tasks;
 
 namespace Common.ECS.Systems
 {
-    class RenderSystem
+    public class RenderSystem : ISystem
     {
+        public List<RenderNode> Targets { get; set; }
+
+        public void Update()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
